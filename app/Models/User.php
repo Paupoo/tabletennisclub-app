6e7e17e07e8865bf2905a9ff6789d84407ccec2b
Aurélien Paulus <aurelien.paulus@gmail.com>
2023-12-12 00:02:48 +0100

    update user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,15 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
         'email',
         'password',
+        'licence',
+        'ranking',
+        'force_index',
+        'team',
+        'role',
     ];
 
     /**

bb416546e0158bf19f927c921abf94bca274cc66
Aurélien Paulus <aurelien.paulus@gmail.com>
2024-08-12 15:17:36 +0200

    first draft show team

--- resources/views/admin/teams/show.blade.php
+++ resources/views/admin/teams/show.blade.php
@@ -17,26 +17,56 @@
     </x-admin-block>
 
     <x-admin-block>
-        @can('update', $team) 
-        <a href="{{ route('teams.edit', $team) }}"><x-primary-button class="my-2 float-end">{{ __('Edit') }}</x-primary-button></a>
+        @can('update', $team)
+            <a href="{{ route('teams.edit', $team) }}"><x-primary-button
+                    class="my-2 float-end">{{ __('Edit') }}</x-primary-button></a>
         @endcan
-        <div class="flex flex-col max-w-sm gap-4 p-10">
+        {{-- <div class="flex flex-col max-w-sm gap-4 p-10">
             <div class="flex justify-between">{{ __('Team name : ') }}<span class="font-bold">{{ $team->name }}</span></div>
             <div class="flex justify-between">{{ __('Season : ') }}<span class="font-bold">{{ $team->season }}</span></div>
             <div class="flex justify-between">{{ __('Division : ') }}<span class="font-bold">{{ $team->division }}</span></div>
             <div class="flex justify-between">{{ __('Players : ') }}</div>
             @isset($team->users)
                 <ol>
-                    @foreach ($team->users->sortBy([
-                        ['ranking', 'asc'], 
-                        ['last_name', 'asc'],
-                        ]) as $player)
+                    @foreach ($team->users->sortBy([['ranking', 'asc'], ['last_name', 'asc']]) as $player)
                         <li>{{ $player->last_name }} {{ $player->first_name }} - {{ $player->ranking }} -- {{ $player->force_index }}</li>
                     @endforeach
                 </ol>
             @endisset
+        </div> --}}
+        <div class="mt-6 w-3/4 h-fit mr-auto border border-gray-100 rounded-md p-1">
+            <div
+                class="w-fit -mt-5 -ml-5 rounded-sm bg-indigo-500 text-white text-lg font-bold text-left py-1 px-3 shadow-md relative">
+                {{ __('Team') }} {{ $team->name }}</div>
+            <div class="w-full m-auto mt-2 rounded-sm bg-white">Hello</div>
+            <div
+                class="w-fit mt-5 -ml-5 rounded-sm bg-indigo-500 text-white text-lg font-bold text-left py-1 px-3 shadow-md relative">
+                {{ __('Captain') }} {{ $team->captain->first_name }} {{ $team->captain->last_name }}</div>
+                <div class="mt-5 border border-gray-200 rounded overflow-hidden shadow-md w-3/4 mx-auto">
+                    {{ $team->captain->first_name }} {{ $team->captain->last_name }}
+                    {{ $team->captain->email }} {{ $team->captain->phone_number }}
+                </div>
+            <div
+                class="w-fit mt-5 -ml-5 rounded-sm bg-indigo-500 text-white text-lg font-bold text-left py-1 px-3 shadow-md relative">
+                {{ __('Players') }}</div>
+            <ul class="mt-5 border border-gray-200 rounded overflow-hidden shadow-md w-3/4 mx-auto">
+                @foreach ($team->users as $player)
+                    <li
+                        class="flex justify-center align-middle gap-1 px-4 py-2 bg-white hover:bg-sky-100 hover:text-sky-900 border-b last:border-none border-gray-200 transition-all duration-300 ease-in-out">
+                        <img class="rounded-full w-12 h-12 border-4 border-indigo-200 object-cover"
+                            @if ($player->sex === \App\Enums\Sex::MEN->name)
+                                src="{{ asset('images/man.png') }}"
+                            @elseif ($player->sex === \App\Enums\Sex::WOMEN->name)
+                                src="{{ asset('images/woman.png') }}"
+                            @endif 
+                        alt="">
+                        <div class="my-auto">
+                            {{ $player->first_name }} {{ $player->last_name }}
+                        </div>
+                    </li>
+                @endforeach
+
+            </ul>
         </div>
     </x-admin-block>
-
-
 </x-app-layout>

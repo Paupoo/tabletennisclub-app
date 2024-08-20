aec9f8b34d76e7672bb9c7ae4ee633cd2aba30b6
Aurélien Paulus <aurelien.paulus@gmail.com>
2024-08-21 00:57:41 +0200

    Interclub first Unit tests & factory

---
+++ database/factories/InterclubFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Interclub;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Interclub>
+ */
+class InterclubFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'address' => 'rue de l\'Invasion 80, 1340 Ottignies',
+            'start_date_time' => "2024-08-12T19:45",
+            'total_players' => 4,
+            'week_number' => 15,
+        ];
+    }
+}

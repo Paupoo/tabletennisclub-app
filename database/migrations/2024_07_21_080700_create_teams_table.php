c50a45f599b1796de2f9ce86302d72c57e4567c1
Aurélien Paulus <aurelien.paulus@gmail.com>
2025-03-17 10:50:02 +0100

    teams table for captain_id column

--- database/migrations/2024_07_21_080700_create_teams_table.php
+++ database/migrations/2024_07_21_080700_create_teams_table.php
@@ -4,6 +4,7 @@ use App\Models\Captain;
 use App\Models\Club;
 use App\Models\League;
 use App\Models\Season;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -20,11 +21,9 @@ return new class extends Migration
             $table->string('name', 1);
             $table->foreignIdFor(League::class)->nullable()->constrained();
             $table->foreignIdFor(Club::class)->nullable()->constrained();
-            $table->unsignedBigInteger('captain_id')->nullable();
+            $table->foreignIdFor(User::class, 'captain_id')->nullable()->constrained();
             $table->foreignIdFor(Season::class)->constrained();
             $table->timestamps();
-
-            $table->foreign('captain_id')->references('id')->on('users');
         });
     }
 

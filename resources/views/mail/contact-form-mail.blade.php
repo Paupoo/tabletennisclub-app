f5610efad57ecda723610c99ec5bce6d189c23ff
Aurélien Paulus <aurelien.paulus@gmail.com>
2025-08-03 20:14:34 +0200

    gestion des pluriel mail de contact et test boutons

--- resources/views/mail/contact-form-mail.blade.php
+++ resources/views/mail/contact-form-mail.blade.php
@@ -12,9 +12,9 @@ Message de :
 
 @if($interest === 'join')
 Demande pour 
-- {{ $membership_family_members }} licence récréative
-- {{ $membership_competitors }} licence compétitive
-- {{ $membership_training_sessions }} séance d'entraînement dirigé
+- {{ $membership_family_members }} licence{{ $membership_family_members > 1 ? 's' : '' }} récréative{{ $membership_family_members > 1 ? 's' : '' }}
+- {{ $membership_competitors }} licence{{ $membership_competitors > 1 ? 's' : '' }} compétitive{{ $membership_competitors > 1 ? 's' : '' }}
+- {{ $membership_training_sessions }} séance{{ $membership_training_sessions > 1 ? 's' : '' }} d'entraînement dirigé
 - Estimation affichée du futur membre : {{ $membership_total_cost }} €
 
 @endif
@@ -23,9 +23,27 @@ Demande pour
                 'membership_competitors' => $this->formData['membership_competitors'] ?? '',
                 'membership_training_sessions' => $this->formData['membership_training_sessions'] ?? '',
                 'membership_total_cost' => $this->formData['membership_total_cost'] ?? '', --}}
-{{-- <x-mail::button :url="''">
-Button Text
-</x-mail::button> --}}
+<x-mail::button :url="'https://www.google.com'">
+Do something
+</x-mail::button>
+<x-mail::button :url="''" color="success">
+Do something else
+</x-mail::button>
+<x-mail::button :url="''" color="error">
+Do something else dangerous
+</x-mail::button>
+
+<x-mail::panel>
+This is the panel content as a test.
+</x-mail::panel>
+
+<x-mail::table>
+| Licence(s) récréatives       | Licence(s) sportive(s)         | Entrainement(s) dirigé(s)       |
+
+| ------------- | :-----------: | ------------: |
+
+| {{ $membership_family_members }}      | {{ $membership_competitors }}       | {{ $membership_training_sessions }}           |
+</x-mail::table>
 
 Envoyé le {{ now()->format('d-m-Y H:i') }},<br>
 {{ config('app.name') }}

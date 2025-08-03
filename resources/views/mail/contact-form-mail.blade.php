4c8718ed04d701e359c7de56981ac635499d029d
Aurélien Paulus <aurelien.paulus@gmail.com>
2025-08-03 21:15:23 +0200

    fix mail layout

--- resources/views/mail/contact-form-mail.blade.php
+++ resources/views/mail/contact-form-mail.blade.php
@@ -1,50 +1,41 @@
 <x-mail::message>
-# Vous avez reçu un message depuis le formulaire de contact du site {{ config('app.name') }}
+Vous avez reçu un message depuis le formulaire de contact du site {{ config('app.name') }}
 
-Message de :
+# Message de :
 
 - Prénom : {{ $first_name }}
 - Nom : {{ $last_name }}
 - Email : {{ $email }}
 - Téléphone : {{ $phone }}
 - Au sujet de : {{ $interest }}
-- Message : {{  $message }}
+ 
+
+# Message : 
+
+<x-mail::panel>{{  $message }}</x-mail::panel>
 
 @if($interest === 'join')
-Demande pour 
-- {{ $membership_family_members }} licence{{ $membership_family_members > 1 ? 's' : '' }} récréative{{ $membership_family_members > 1 ? 's' : '' }}
-- {{ $membership_competitors }} licence{{ $membership_competitors > 1 ? 's' : '' }} compétitive{{ $membership_competitors > 1 ? 's' : '' }}
-- {{ $membership_training_sessions }} séance{{ $membership_training_sessions > 1 ? 's' : '' }} d'entraînement dirigé
-- Estimation affichée du futur membre : {{ $membership_total_cost }} €
 
-@endif
-                
-                {{-- 'membership_family_members' => $this->formData['membership_family_members'] ?? '',
-                'membership_competitors' => $this->formData['membership_competitors'] ?? '',
-                'membership_training_sessions' => $this->formData['membership_training_sessions'] ?? '',
-                'membership_total_cost' => $this->formData['membership_total_cost'] ?? '', --}}
-<x-mail::button :url="'https://www.google.com'">
-Do something
-</x-mail::button>
-<x-mail::button :url="''" color="success">
-Do something else
-</x-mail::button>
-<x-mail::button :url="''" color="error">
-Do something else dangerous
-</x-mail::button>
-
-<x-mail::panel>
-This is the panel content as a test.
-</x-mail::panel>
+# Récapitulatif de la demande :
 
 <x-mail::table>
-| Licence(s) récréatives       | Licence(s) sportive(s)         | Entrainement(s) dirigé(s)       |
-
-| ------------- | :-----------: | ------------: |
-
-| {{ $membership_family_members }}      | {{ $membership_competitors }}       | {{ $membership_training_sessions }}           |
+| Demande                                                                                                                   | Total                                 |
+| -------------                                                                                                             | ------------:                         |
+| Licence{{ $membership_family_members > 1 ? 's' : '' }} récréative{{ $membership_family_members > 1 ? 's' : '' }}          | {{ $membership_family_members }}      |
+| Licence{{ $membership_competitors > 1 ? 's' : '' }} compétitive{{ $membership_competitors > 1 ? 's' : '' }}               | {{ $membership_competitors }}         |
+| Entraînement{{ $membership_training_sessions > 1 ? 's' : '' }} dirigé{{ $membership_training_sessions > 1 ? 's' : '' }}   | {{ $membership_training_sessions }}   |
 </x-mail::table>
+@endif
 
-Envoyé le {{ now()->format('d-m-Y H:i') }},<br>
-{{ config('app.name') }}
+<x-mail::button :url="'https://www.google.com'">
+Gérer la demande
+</x-mail::button>
+<x-mail::button :url="''" color="success">
+Clôturer la demande positivement
+</x-mail::button>
+<x-mail::button :url="''" color="error">
+Rejeter/marquer comme spam
+</x-mail::button>
+
+Envoyé le {{ now()->format('d-m-Y H:i') }}<br>
 </x-mail::message>

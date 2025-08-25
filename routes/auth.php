8d83960cb19e66ca5f92aea47e1a11ed4c42c939
Aurélien Paulus <aurelien.paulus@gmail.com>
2025-08-25 16:50:27 +0200

    route registration to antispam middleware

--- routes/auth.php
+++ routes/auth.php
@@ -11,13 +11,15 @@ use App\Http\Controllers\Auth\PasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
+use App\Http\Middleware\ProtectAgainstSpam;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function (): void {
     Route::get('register', [RegisteredUserController::class, 'create'])
         ->name('register');
 
-    Route::post('register', [RegisteredUserController::class, 'store']);
+    Route::post('register', [RegisteredUserController::class, 'store'])
+        ->middleware(ProtectAgainstSpam::class);
 
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
         ->name('login');

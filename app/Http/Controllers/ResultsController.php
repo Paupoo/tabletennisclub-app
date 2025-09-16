f2e684a190b5a07b94f271aaa1b18af8b2f25a11
Aurélien Paulus <aurelien.paulus@gmail.com>
2025-09-16 10:21:14 +0200

    add results week 1 2025

--- app/Http/Controllers/ResultsController.php
+++ app/Http/Controllers/ResultsController.php
@@ -13,34 +13,55 @@ class ResultsController extends Controller
         $selectedSeason = $request->get('season', '2024');
         $seasons = ['2025-2026', '2024-2025', '2023-2024'];
 
-        // $teams = [
-        //     [
-        //         'name' => 'Équipe A - Division Premier',
-        //         'position' => '2ème Place',
-        //         'position_class' => 'bg-green-100 text-green-800',
-        //         'matches' => [
-        //             ['date' => '15 Déc 2024', 'opponent' => 'Thunder TTC', 'venue' => 'Domicile', 'score' => '8-2', 'result' => 'Victoire'],
-        //             ['date' => '8 Déc 2024', 'opponent' => 'Elite Paddles', 'venue' => 'Extérieur', 'score' => '6-4', 'result' => 'Victoire'],
-        //             ['date' => '1 Déc 2024', 'opponent' => 'Spin Masters', 'venue' => 'Domicile', 'score' => '3-7', 'result' => 'Défaite'],
-        //             ['date' => '24 Nov 2024', 'opponent' => 'Rapid Rackets', 'venue' => 'Extérieur', 'score' => '7-3', 'result' => 'Victoire'],
-        //         ],
-        //         'stats' => ['played' => 12, 'wins' => 9, 'losses' => 3, 'win_rate' => 75]
-        //     ],
-        //     [
-        //         'name' => 'Équipe B - Division 1',
-        //         'position' => '1ère Place',
-        //         'position_class' => 'bg-club-yellow text-club-blue',
-        //         'matches' => [
-        //             ['date' => '14 Déc 2024', 'opponent' => 'City Spinners', 'venue' => 'Domicile', 'score' => '9-1', 'result' => 'Victoire'],
-        //             ['date' => '7 Déc 2024', 'opponent' => 'Paddle Power', 'venue' => 'Extérieur', 'score' => '8-2', 'result' => 'Victoire'],
-        //             ['date' => '30 Nov 2024', 'opponent' => 'Net Ninjas', 'venue' => 'Domicile', 'score' => '7-3', 'result' => 'Victoire'],
-        //             ['date' => '23 Nov 2024', 'opponent' => 'Smash Squad', 'venue' => 'Extérieur', 'score' => '6-4', 'result' => 'Victoire'],
-        //         ],
-        //         'stats' => ['played' => 10, 'wins' => 10, 'losses' => 0, 'win_rate' => 100]
-        //     ]
-        // ];
+        $teams = [
+            [
+                'name' => 'Équipe A - Division 2C',
+                'position' => '1ère Place',
+                'position_class' => 'bg-green-100 text-green-800',
+                'matches' => [
+                    ['date' => '12 Sep 2025', 'opponent' => 'Arc En Ciel F', 'venue' => 'Domicile', 'score' => '15-1', 'result' => 'Victoire'],
+                ],
+                'stats' => ['played' => 1, 'wins' => 1, 'losses' => 0, 'win_rate' => 100]
+            ],
+            [
+                'name' => 'Équipe B - Division 3B',
+                'position' => '5ème Place',
+                'position_class' => 'bg-yellow-100 text-yellow-800',
+                'matches' => [
+                    ['date' => '12 Sep 2025', 'opponent' => 'Arc En Ciel G', 'venue' => 'Domicile', 'score' => '8-8', 'result' => 'Nul'],
+                ],
+                'stats' => ['played' => 1, 'wins' => 0, 'losses' => 0, 'win_rate' => 0]
+            ],
+            [
+                'name' => 'Équipe C - Division 4C',
+                'position' => '2ème Place',
+                'position_class' => 'bg-green-100 text-green-800',
+                'matches' => [
+                    ['date' => '12 Sep 2025', 'opponent' => 'CTT Limal Wavre F', 'venue' => 'Extérieur', 'score' => '13-3', 'result' => 'Victoire'],
+                ],
+                'stats' => ['played' => 1, 'wins' => 1, 'losses' => 0, 'win_rate' => 100]
+            ],
+            [
+                'name' => 'Équipe D - Division 4D',
+                'position' => '1ère Place',
+                'position_class' => 'bg-green-100 text-green-800',
+                'matches' => [
+                    ['date' => '13 Sep 2025', 'opponent' => 'CTT Le Moulin C', 'venue' => 'Extérieur', 'score' => '14-2', 'result' => 'Victoire'],
+                ],
+                'stats' => ['played' => 1, 'wins' => 1, 'losses' => 0, 'win_rate' => 100]
+            ],
+            [
+                'name' => 'Équipe E - Division 5H',
+                'position' => '5ème Place',
+                'position_class' => 'bg-red-100 text-red-800',
+                'matches' => [
+                    ['date' => '12 Sep 2025', 'opponent' => 'CTT Alpa Schaerbeek P', 'venue' => 'Extérieur', 'score' => '6-10', 'result' => 'Défaite'],
+                ],
+                'stats' => ['played' => 1, 'wins' => 0, 'losses' => 1, 'win_rate' => 0]
+            ],
+        ];
 
-        $teams = [];
+        // $teams = [];
 
         return view('public.results', compact('teams', 'seasons', 'selectedSeason'));
     }

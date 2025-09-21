de60a03c51a72aa83bbd878da9cf3ae2922d2b9f
Aurélien Paulus <aurelien.paulus@gmail.com>
2025-09-21 10:46:28 +0200

    update sponsor (cf mail Manon)

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,8 +14,8 @@ class HomeController extends Controller
             // ['name' => 'Eric Filée', 'logo' => asset('images/sponsors/ericfilee.png'), 'url' => null],
             // ['name' => 'GD Tax & Account', 'logo' => asset('images/sponsors/gd_tax_account.png'), 'url' => null],
             // ['name' => 'La maison de Malou', 'logo' => asset('images/sponsors/malou.png'), 'url' => 'https://www.lamaisondemalou.be/'],
-            ['name' => 'La maison de Malou', 'logo' => asset('images/sponsors/logo_10x102.jpg'), 'url' => 'https://www.lamaisondemalou.be/'],
-            ['name' => 'Chatisfait', 'logo' => asset('images/sponsors/banderole.jpg'), 'url' => 'https://www.chatisfait.be/'],
+            ['name' => 'La maison de Malou', 'logo' => asset('images/sponsors/sponsor_1_v2.jpg'), 'url' => 'https://www.lamaisondemalou.be/'],
+            ['name' => 'Chatisfait', 'logo' => asset('images/sponsors/sponsor_2_v2.png'), 'url' => 'https://www.chatisfait.be/'],
             // ['name' => 'Artadom SPRL', 'logo' => null, 'url' => null],
         ];
 

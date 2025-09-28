164d484f9b29ef63d0d4f24bf89834bd2c748c15
Aurélien Paulus <aurelien.paulus@gmail.com>
2025-09-28 22:10:00 +0200

    micro fix victory rate colour

--- app/Http/Controllers/ResultsController.php
+++ app/Http/Controllers/ResultsController.php
@@ -20,9 +20,10 @@ class ResultsController extends Controller
                 'position_class' => 'bg-green-100 text-green-800',
                 'matches' => [
                     ['date' => '12 Sep 2025', 'opponent' => 'Arc En Ciel F', 'venue' => 'Domicile', 'score' => '15-1', 'result' => 'Victoire'],
-                    ['date' => '21 Sep 2025', 'opponent' => 'TT Zenith Brussels A', 'venue' => 'Extérieur', 'score' => 'Indisponible', 'result' => 'Indisponible'],
+                    ['date' => '21 Sep 2025', 'opponent' => 'TT Zenith Brussels A', 'venue' => 'Extérieur', 'score' => '7-9', 'result' => 'Victoire'],
+                    ['date' => '26 Sep 2025', 'opponent' => 'Braine l\'Alleud I', 'venue' => 'Domicile', 'score' => '14-2', 'result' => 'Victoire'],
                 ],
-                'stats' => ['played' => 1, 'wins' => 1, 'losses' => 0, 'win_rate' => 100]
+                'stats' => ['played' => 3, 'wins' => 3, 'losses' => 0, 'win_rate' => 100]
             ],
             [
                 'name' => 'Équipe B - Division 3B',
@@ -31,8 +32,9 @@ class ResultsController extends Controller
                 'matches' => [
                     ['date' => '12 Sep 2025', 'opponent' => 'Arc En Ciel G', 'venue' => 'Domicile', 'score' => '8-8', 'result' => 'Nul'],
                     ['date' => '19 Sep 2025', 'opponent' => 'REP Nivelles D', 'venue' => 'Extérieur', 'score' => '7-9', 'result' => 'Victoire'],
+                    ['date' => '26 Sep 2025', 'opponent' => 'Piranha G', 'venue' => 'Domicile', 'score' => '11-5', 'result' => 'Victoire'],
                 ],
-                'stats' => ['played' => 2, 'wins' => 1, 'losses' => 0, 'win_rate' => 50]
+                'stats' => ['played' => 3, 'wins' => 2, 'losses' => 0, 'win_rate' => 66]
             ],
             [
                 'name' => 'Équipe C - Division 4C',
@@ -41,8 +43,9 @@ class ResultsController extends Controller
                 'matches' => [
                     ['date' => '12 Sep 2025', 'opponent' => 'CTT Limal Wavre F', 'venue' => 'Extérieur', 'score' => '13-3', 'result' => 'Victoire'],
                     ['date' => '19 Sep 2025', 'opponent' => 'Safran A', 'venue' => 'Domicile', 'score' => '6-10', 'result' => 'Défaite'],
+                    ['date' => '27 Sep 2025', 'opponent' => 'Braine l\'Alleud M', 'venue' => 'Extérieur', 'score' => '3-13', 'result' => 'Victoire'],
                 ],
-                'stats' => ['played' => 2, 'wins' => 1, 'losses' => 1, 'win_rate' => 50]
+                'stats' => ['played' => 3, 'wins' => 2, 'losses' => 1, 'win_rate' => 66]
             ],
             [
                 'name' => 'Équipe D - Division 4D',
@@ -51,8 +54,9 @@ class ResultsController extends Controller
                 'matches' => [
                     ['date' => '13 Sep 2025', 'opponent' => 'CTT Le Moulin C', 'venue' => 'Extérieur', 'score' => '14-2', 'result' => 'Victoire'],
                     ['date' => '13 Sep 2025', 'opponent' => 'TT Zenith Brussels C', 'venue' => 'Domicile', 'score' => '1-15', 'result' => 'Défaite'],
+                    ['date' => '27 Sep 2025', 'opponent' => 'Gremlins F', 'venue' => 'Extérieur', 'score' => '5-11', 'result' => 'Victoire'],
                 ],
-                'stats' => ['played' => 1, 'wins' => 1, 'losses' => 1, 'win_rate' => 50]
+                'stats' => ['played' => 3, 'wins' => 2, 'losses' => 1, 'win_rate' => 66]
             ],
             [
                 'name' => 'Équipe E - Division 5H',
@@ -61,8 +65,9 @@ class ResultsController extends Controller
                 'matches' => [
                     ['date' => '12 Sep 2025', 'opponent' => 'CTT Alpa Schaerbeek P', 'venue' => 'Extérieur', 'score' => '6-10', 'result' => 'Défaite'],
                     ['date' => 'Bye', 'opponent' => 'Bye', 'venue' => 'Domicile', 'score' => 'Bye', 'result' => 'Bye'],
+                    ['date' => '26 Sep 2025', 'opponent' => 'Tourinnes E', 'venue' => 'Extérieur', 'score' => '0-16', 'result' => 'Défaite'],
                 ],
-                'stats' => ['played' => 1, 'wins' => 0, 'losses' => 1, 'win_rate' => 0]
+                'stats' => ['played' => 2, 'wins' => 0, 'losses' => 2, 'win_rate' => 0]
             ],
         ];
         

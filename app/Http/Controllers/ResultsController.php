39207dec87ccaa31487b9c930c5be5c696fbfb0b
Aurélien Paulus <aurelien.paulus@gmail.com>
2025-11-29 23:25:48 +0100

    results updates week 9 2025

--- app/Http/Controllers/ResultsController.php
+++ app/Http/Controllers/ResultsController.php
@@ -27,8 +27,9 @@ class ResultsController extends Controller
                     ['date' => '7 Nov 2025', 'opponent' => 'Tourinnes A', 'venue' => 'Extérieur', 'score' => '9-7', 'result' => 'Défaite'],
                     ['date' => '14 Nov 2025', 'opponent' => 'La Hulpe-Rix. C', 'venue' => 'Extérieur', 'score' => '4-12', 'result' => 'Victoire'],
                     ['date' => '21 Nov 2025', 'opponent' => 'Set-Jet Fleur Bleue D', 'venue' => 'Domicile', 'score' => '13-3', 'result' => 'Victoire'],
+                    ['date' => '28 Nov 2025', 'opponent' => 'Eveil B', 'venue' => 'Domicile', 'score' => '16-0', 'result' => 'Victoire'],
                 ],
-                'stats' => ['played' => 8, 'wins' => 7, 'losses' => 1, 'win_rate' => 88]
+                'stats' => ['played' => 9, 'wins' => 8, 'losses' => 1, 'win_rate' => 89]
             ],
             [
                 'name' => 'Équipe B - Division 3B',
@@ -43,8 +44,9 @@ class ResultsController extends Controller
                     ['date' => '7 Nov 2025', 'opponent' => 'Logis Auderghem N', 'venue' => 'Extérieur', 'score' => '6-10', 'result' => 'Victoire'],
                     ['date' => '14 Nov 2025', 'opponent' => 'La Hulpe Rix. D', 'venue' => 'Extérieur', 'score' => '11-5', 'result' => 'Défaite'],
                     ['date' => '21 Nov 2025', 'opponent' => 'Ry Ternel C', 'venue' => 'Domicile', 'score' => '5-11', 'result' => 'Défaite'],
+                    ['date' => '28 Nov 2025', 'opponent' => 'Eveil D', 'venue' => 'Domicile', 'score' => '11-5', 'result' => 'Victoire'],
                 ],
-                'stats' => ['played' => 8, 'wins' => 4, 'losses' => 3, 'win_rate' => 50]
+                'stats' => ['played' => 9, 'wins' => 5, 'losses' => 3, 'win_rate' => 56]
             ],
             [
                 'name' => 'Équipe C - Division 4C',
@@ -59,8 +61,9 @@ class ResultsController extends Controller
                     ['date' => '7 Nov 2025', 'opponent' => 'Beauchamp A', 'venue' => 'Domicile', 'score' => '11-5', 'result' => 'Victoire'],
                     ['date' => '14 Nov 2025', 'opponent' => 'Witterzee A', 'venue' => 'Domicile', 'score' => '2-14', 'result' => 'Défaite'],
                     ['date' => '21 Nov 2025', 'opponent' => 'Set-Jet Fleur Bleue J', 'venue' => 'Extérieur', 'score' => '3-13', 'result' => 'Victoire'],
+                    ['date' => '29 Nov 2025', 'opponent' => 'Logis Auderghem S', 'venue' => 'Extérieur', 'score' => '10-6', 'result' => 'Défaite'],
                 ],
-                'stats' => ['played' => 8, 'wins' => 5, 'losses' => 3, 'win_rate' => 63]
+                'stats' => ['played' => 9, 'wins' => 5, 'losses' => 4, 'win_rate' => 56]
             ],
             [
                 'name' => 'Équipe D - Division 4D',
@@ -75,8 +78,9 @@ class ResultsController extends Controller
                     ['date' => '7 Nov 2025', 'opponent' => 'Arc En Ciel J', 'venue' => 'Domicile', 'score' => '5-11', 'result' => 'Défaite'],
                     ['date' => '14 Nov 2025', 'opponent' => 'Smash Evere C', 'venue' => 'Domicile', 'score' => '10-6', 'result' => 'Victoire'],
                     ['date' => '21 Nov 2025', 'opponent' => 'Set-Jet Fleur Bleue F', 'venue' => 'Extérieur', 'score' => '6-10', 'result' => 'Défaite'],
+                    ['date' => '28 Nov 2025', 'opponent' => 'Logis Auderghem Q', 'venue' => 'Extérieur', 'score' => '13-3', 'result' => 'Défaite'],
                 ],
-                'stats' => ['played' => 8, 'wins' => 4, 'losses' => 4, 'win_rate' => 50]
+                'stats' => ['played' => 9, 'wins' => 4, 'losses' => 5, 'win_rate' => 44]
             ],
             [
                 'name' => 'Équipe E - Division 5H',
@@ -91,6 +95,7 @@ class ResultsController extends Controller
                     ['date' => '7 Nov 2025', 'opponent' => 'Limal Wavre I', 'venue' => 'Domicile', 'score' => '4-12', 'result' => 'Défaite'],
                     ['date' => '14 Nov 2025', 'opponent' => 'Royal Clabecq G', 'venue' => 'Domicile', 'score' => '4-12', 'result' => 'Défaite'],
                     ['date' => '21 Nov 2025', 'opponent' => 'REP Nivelles H', 'venue' => 'Extérieur', 'score' => '3-13', 'result' => 'Victoire'],
+                    ['date' => 'Bye', 'opponent' => 'Bye', 'venue' => 'Domicile', 'score' => 'Bye', 'result' => 'Bye'],
                 ],
                 'stats' => ['played' => 7, 'wins' => 1, 'losses' => 6, 'win_rate' => 14]
             ],

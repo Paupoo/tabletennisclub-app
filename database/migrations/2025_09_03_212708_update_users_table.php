a5ecdabe17a19ecbb218fe6882b8e929c6481c57
Aurélien Paulus <aurelien.paulus@gmail.com>
2026-02-01 21:09:24 +0100

    fix migration

--- database/migrations/2025_09_03_212708_update_users_table.php
+++ database/migrations/2025_09_03_212708_update_users_table.php
@@ -5,28 +5,50 @@ declare(strict_types=1);
 use App\Enums\Sex;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
 
 return new class extends Migration
 {
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::table('users', function (Blueprint $table) {
-            $table->string('password', 255)->nullable()->change();
-        });
-    }
-
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
+            // Rend le mot de passe optionnel pour permettre une invitation par mail, sans mot de passe pour la première connexion.
             $table->string('password', 255)->nullable()->change();
+            // Met à jour la définition de l'enum (l'Enum PHP a changé, ajout d'un nouveau case, mise à jour de la DB)
             $table->enum('sex', array_column(Sex::cases(), 'name'))->default(Sex::MEN->name)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        // 1. On récupère tous les IDs des utilisateurs qui n'ont pas de mot de passe
+        $usersWithoutPassword = DB::table('users')->whereNull('password')->pluck('id');
+
+        // 2. Pour chaque utilisateur, on génère un mot de passe unique
+        foreach ($usersWithoutPassword as $id) {
+            DB::table('users')
+                ->where('id', $id)
+                ->update([
+                    'password' => Hash::make(Str::random(12))
+                ]);
+        }
+
+        // 3. On remet la contrainte NOT NULL et on remet à jour les Enums.
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('password', 255)->nullable(false)->change();
+
+            $table->enum('sex', array_column(Sex::cases(), 'name'))
+                ->default(Sex::MEN->name)
+                ->change();
         });
     }
 };

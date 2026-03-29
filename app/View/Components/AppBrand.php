7b4a6da7264442147729bb38511f65a87bb4e93c
Aurélien Paulus <aurelien.paulus@gmail.com>
2026-03-29 19:19:19 +0200

    fix logo's color with dark mode

--- app/View/Components/AppBrand.php
+++ app/View/Components/AppBrand.php
@@ -29,8 +29,8 @@ class AppBrand extends Component
                     <!-- Hidden when collapsed -->
                     <div {{ $attributes->class(["hidden-when-collapsed"]) }}>
                         <div class="flex flex-col items-center shrink-0 gap-2">
-                            <x-logo class="block w-auto text-club-blue fill-current h-9 dark:text-gray-200 group-hover:text-club-blue-light transition-colors duration-200" />
-                            <span class="hidden sm:block ml-4 text-lg font-bold text-club-blue dark:text-gray-200 group-hover:text-club-blue-light transition-colors duration-200">
+                            <x-logo class="block w-auto text-primary fill-current h-9" />
+                            <span class="hidden sm:block ml-4 text-lg font-bold text-primary">
                                 {{ config('app.name', 'Club') }}
                             </span>
                         </div>
@@ -38,7 +38,7 @@ class AppBrand extends Component
 
                     <!-- Display when collapsed -->
                     <div class="display-when-collapsed hidden mx-5 mt-5 mb-1 h-[28px]">
-                        <x-logo class="block w-auto text-club-blue fill-current h-9 dark:text-gray-200 group-hover:text-club-blue-light transition-colors duration-200" />
+                        <x-logo class="block w-auto text-primary fill-current h-9" />
                     </div>
                 </a>
 

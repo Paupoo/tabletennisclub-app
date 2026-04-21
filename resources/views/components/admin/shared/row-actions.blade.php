c3ac551f5d5e6647cf1f13075d2a586db334ce08
Aurélien Paulus <aurelien.paulus@gmail.com>
2026-04-21 21:52:44 +0200

    fix actions in row component (bug truncated menu)

--- resources/views/components/admin/shared/row-actions.blade.php
+++ resources/views/components/admin/shared/row-actions.blade.php
@@ -3,11 +3,34 @@
     'icon' => 'o-ellipsis-vertical',
 ])
 
-<x-dropdown right>
-    <x-slot:trigger>
-        <x-button {{ $attributes->merge(['class' => 'btn-sm btn-ghost']) }} icon="{{ $icon }}"
-            label="{{ $label }}" />
-    </x-slot:trigger>
+<div x-data="{ open: false }" class="inline-flex items-center">
+    {{-- Trigger minimaliste --}}
+    <button
+        x-ref="trigger"
+        @click="open = !open"
+        type="button"
+        {{ $attributes->merge(['class' => 'p-1 text-gray-400 hover:text-main-500 rounded-full hover:bg-base-200 transition-all']) }}
+    >
+        <x-icon :name="$icon" class="w-4 h-4" />
+    </button>
 
-    {{ $slot }}
-</x-dropdown>
+    {{-- Le Menu téléporté --}}
+    <template x-teleport="body">
+        <div
+            x-show="open"
+            x-cloak
+            @click.outside="open = false"
+            x-anchor.bottom-end.offset.2="$refs.trigger"
+            x-transition.opacity
+            {{-- Ici, on utilise les classes de MaryUI pour le look 'dropdown' --}}
+            class="absolute z-[9999] shadow-lg border border-base-200 bg-base-100 rounded-lg min-w-[150px]"
+        >
+            {{-- On réutilise x-menu pour que x-menu-item retrouve son style (hover, padding, etc) --}}
+            <x-menu dynamic class="p-1">
+                <div @click="open = false">
+                    {{ $slot }}
+                </div>
+            </x-menu>
+        </div>
+    </template>
+</div>
